3caa0919ee8da7cf5bf85d2ced76e9dc500285d5
Chain <chensunmac@gmail.com>
2017-10-10 15:40:32 -0400

    longest substring with no repeated character

---
+++ python/3_Longest Substring Without Repeating Characters.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 15:28:57 2017
+
+@author: chens
+"""
+
+class Solution:
+    # @return an integer
+    def lengthOfLongestSubstring(self, s):
+        start = maxLength = 0
+        usedChar = {}
+        
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:
+                start = usedChar[s[i]] + 1
+            else:
+                maxLength = max(maxLength, i - start + 1)
+
+            usedChar[s[i]] = i
+
+        return maxLength
+
+if __name__ == "__main__":
+    assert Solution().lengthOfLongestSubstring("abceads") == 6
